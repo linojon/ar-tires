9fa94a6ee44e3b4df73f8fde0da5113c6686222c
Jonathan Linowes <jonathan@parkerhill.com>
2017-05-21 03:48:19 -0400

    show green circle to capture, hide capture ui when tracking

---
+++ Assets/ChangeTire/Scripts/TrackableObjectVisibility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+[RequireComponent(typeof(TrackableBehaviour))]
+public class TrackableObjectVisibility : MonoBehaviour, ITrackableEventHandler {
+
+    public GameObject Target;
+    public bool ShowOnFound = true;
+
+    private TrackableBehaviour trackableBehaviour;
+
+    void Start() {
+        trackableBehaviour = GetComponent<TrackableBehaviour>();
+        trackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus) {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+            OnTargetFound();
+        } else {
+            OnTargetLost();
+        }
+    }
+
+    void OnTargetFound() {
+        if (Target) {
+            Target.SetActive(ShowOnFound);
+        }
+    }
+
+    void OnTargetLost() {
+        if (Target) {
+            Target.SetActive(!ShowOnFound);
+        }
+    }
+}
